f04c7141a95e9671a19c29feeeebe6c71ee44046
User0809 <agievicdaria@gmail.com>
2023-11-18 23:32:08 +0200

    update

--- lab_6.4-it.cpp
+++ lab_6.4-it.cpp
@@ -1,3 +1,110 @@
+// #include <iostream>
+// #include <cmath>
+// #include <iomanip>
+
+// using namespace std;
+
+// void findMinMaxIndices(int array[], int n, int& minIndex, int& maxIndex) {
+//     minIndex = 0;
+//     maxIndex = 0;
+//     for (int i = 1; i < n; ++i) {
+//         if (array[i] > array[maxIndex]) {
+//             maxIndex = i;
+//         }
+//         if (array[i] < array[minIndex]) {
+//             minIndex = i;
+//         }
+//     }
+// }
+
+// int calculateProduct(int array[], int startIndex, int endIndex) {
+//     int product = 1;
+//     for (int i = startIndex; i < endIndex; ++i) {
+//         product *= array[i];
+//     }
+//     return product;
+// }
+
+// void inputArray(int*& array, int n) {
+//     array = new int[n];
+//     cout << "Введіть елементи масиву:" << endl;
+//     for (int i = 0; i < n; ++i)  {
+//         cout << "a[" << i << "]= ? ";
+//         cin >> array[i];
+//     }
+// }
+
+
+// int calculateStartIndex(int minIndex, int maxIndex) {
+//     return (minIndex < maxIndex) ? (minIndex + 1) : (maxIndex + 1);
+// }
+
+// int calculateEndIndex(int minIndex, int maxIndex) {
+//     return (minIndex > maxIndex) ? minIndex : maxIndex;
+// }
+
+// // void printArray(int array[], int n) {
+// //     cout << "Масив: ";
+// //     for (int i = 0; i < n; ++i) {
+// //         cout << array[i] << " ";
+// //     }
+// //     cout << endl;
+// // }
+
+// void printArray(int array[], int n) {
+//     cout << "Масив: ";
+//     for (int i = 0; i < n; ++i) {
+//         cout << setw(4) << array[i] << " ";
+//     }
+//     cout << endl;
+// }
+
+// void sortEvenElements(int array[], int size) {
+//     for (int i = 0; i < size; ++i) {
+//         if (array[i] % 2 == 0) {
+//             for (int j = i + 1; j < size; ++j) {
+//                 if (array[j] % 2 == 0 && array[j] < array[i]) {
+//                     int temp = array[i];
+//                     array[i] = array[j];
+//                     array[j] = temp;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// int main() {
+//     int n;
+//     cout << "Введіть кількість елементів у масиві: ";
+//     cin >> n;
+
+//     int *array = new int[n];
+
+//     inputArray(array, n);
+
+//     int minIndex, maxIndex;
+//     findMinMaxIndices(array, n, minIndex, maxIndex);
+
+//     int startIndex = calculateStartIndex(minIndex, maxIndex);
+//     int endIndex = calculateEndIndex(minIndex, maxIndex);
+
+//     int product = calculateProduct(array, startIndex, endIndex);
+
+//     printArray(array, n);    
+
+//     sortEvenElements(array, n);
+//     cout << "Масив після впорядкування парних елементів за зростанням: ";
+//     cout << endl;
+//     printArray(array, n);
+
+//     cout << "Добуток елементів між максимальним та мінімальним елементами: " << product << endl;
+
+//     delete[] array;
+
+//     return 0;
+// }
+
+//!
 #include <iostream>
 #include <cmath>
 #include <iomanip>
@@ -25,10 +132,21 @@ int calculateProduct(int array[], int startIndex, int endIndex) {
     return product;
 }
 
+int calculateSumOfNegatives(int array[], int n) {
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        if (array[i] < 0) {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
 void inputArray(int*& array, int n) {
     array = new int[n];
     cout << "Введіть елементи масиву:" << endl;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i)  {
+        cout << "a[" << i << "]= ? ";
         cin >> array[i];
     }
 }
@@ -44,7 +162,7 @@ int calculateEndIndex(int minIndex, int maxIndex) {
 void printArray(int array[], int n) {
     cout << "Масив: ";
     for (int i = 0; i < n; ++i) {
-        cout << array[i] << " ";
+        cout << setw(4) << array[i] << " ";
     }
     cout << endl;
 }
@@ -69,7 +187,6 @@ int main() {
     cin >> n;
 
     int *array = new int[n];
-
     inputArray(array, n);
 
     int minIndex, maxIndex;
@@ -79,14 +196,17 @@ int main() {
     int endIndex = calculateEndIndex(minIndex, maxIndex);
 
     int product = calculateProduct(array, startIndex, endIndex);
+    int sumOfNegatives = calculateSumOfNegatives(array, n);
 
-    printArray(array, n);    
-
+    printArray(array, n);
+    
     sortEvenElements(array, n);
     cout << "Масив після впорядкування парних елементів за зростанням: ";
+    cout << endl;
     printArray(array, n);
 
     cout << "Добуток елементів між максимальним та мінімальним елементами: " << product << endl;
+    cout << "Сума від'ємних елементів у масиві: " << sumOfNegatives << endl;
 
     delete[] array;
 
